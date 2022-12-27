8d58add1010975bafcac22fd0223c69b885e0e3f
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-27 15:03:30 +0700

    feat: learn router dom
    add router index and clean up code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import React, {useState, useEffect} from 'react';
-import Introduction from './components/introduction';
-import Profile from './components/profile';
-import Footer from './components/footer';
+// Import Components
+import Router from "./router";
+import Navbar from "./components/navbar";
 
 function App() {
-  const profileHeading = "Belajar React";
-  const profileText = "Belajar syntax dasar react js";
-
-  const footer = () =>  {
-    return (
-      <div>
-        <marquee>
-          Selamat belajar,
-          <br/>
-          Sayang zooane dehh
-        </marquee>
-      </div>
-    );
-  }
-
-  const [text, setText] = useState("Benjamin4k");
-
-  useEffect(() => {
-    text == '' ? setText("Benjamin4k") : setText(text);
-  }, [text]);
-
   return (
     <div>
-      <Profile heading={profileHeading} text={profileText} />
-      <Introduction name={text} />
-      <input type="text" className='text' placeholder='Nama kamu' onChange={(typing) => setText(typing.target.value)} />
-      <Footer footer={footer} />
+      <Navbar />
+      <div className="py-4">
+        <Router />
+      </div>
     </div>
   );
 }
