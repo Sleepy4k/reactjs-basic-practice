2d40ae3639ed14b93c955eab9fd7e3a7954b8151
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2022-12-27 12:40:24 +0700

    feat: practice-1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Introduction from './components/introduction';
+import Profile from './components/profile';
+
+function App() {
+  return (
+    <div>
+      <Profile />
+      <Introduction name="Benjamin4k" />
+      <Introduction name="Zooane" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/profile.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Profile extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Belajar React</h1>
+        <p>Belajar syntax dasar react js</p>
+      </div>
+    )
+  }
+}
+
+export default Profile
